0aad8cae32de2208f07bc56d55fa31cf1278284b
Alex Bezpalko <alexbe@wix.com>
2017-09-20 10:57:54 +0300

    order functions and cleanup

--- e4/e4.py
+++ e4/e4.py
@@ -40,20 +40,6 @@ def json_serial(obj):
     raise TypeError("Type %s not serializable" % type(obj))
 
 
-#@app.teardown_appcontext
-#def close_db(error):
-#    """Closes the database again at the end of the request."""
-#    if hasattr(g, 'sqlite_db'):
-#       g.sqlite_db.close()
-
-
-#@app.cli.command('initdb')
-#def initdb_command():
-#    """Initializes the database."""
-#    init_db()
-#    print('Initialized the database.')
-
-
 @app.cli.command('rates')
 @app.route('/update_rates')
 def update_rates():
@@ -107,40 +93,6 @@ def show_incomes():
                            )
 
 
-@app.route('/income/modify', methods=['POST'])
-def add_entry():
-
-    if not session.get('logged_in'):
-        abort(401)
-    try:
-        current_income = DB.query(Income).get(int(request.form['hidden_id']))
-        current_income.title = request.form['title']
-        current_income.currency_id = int(request.form['currency'])
-        current_income.sum = int(request.form['sum']*CURRENCY_SCALE)
-        current_income.start_date = datetime.datetime.strptime(
-            request.form['start_date'], '%Y-%m-%d').date()
-        current_income.end_date = (None if request.form['end_date'] == '' else atetime.datetime.strptime(
-            request.form['end_date'], '%Y-%m-%d').date())
-        current_income.period_id = int(request.form['period'])
-
-    except:
-        current_income = Income(
-            id=int(request.form['period']),
-            title=request.form['title'],
-            currency_id=int(request.form['currency']),
-            sum=inf(request.form['sum']*CURRENCY_SCALE),
-            start_date=datetime.datetime.strptime(
-                request.form['start_date'], '%Y-%m-%d').date(),
-            end_date=(None if request.form['end_date'] == '' else atetime.datetime.strptime(
-                request.form['end_date'], '%Y-%m-%d').date()),
-            period_id=int(request.form['period']))
-        DB.add(current_income)
-
-    DB.commit()
-    #flash('New entry was successfully posted')
-    return redirect(url_for('show_incomes'))
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -177,77 +129,82 @@ def currency_GET(*args, **kwargs):
             entries.append(rate.to_dict())
     return entries
 
-
-def income_GET(*args, **kwargs):
-    data = DB.query(Income).all() if kwargs['id'] == 0 else DB.query(Income).get(kwargs['id'])
-    return data
-
-
 def income_DELETE(*args, **kwargs):
     id = kwargs['id']
     income = DB.query(Income).filter_by(id=id).delete()
     DB.commit()
     return {'deleted': id}
 
+def income_GET(*args, **kwargs):
+    data = DB.query(Income).all() if kwargs['id'] == 0 else DB.query(Income).get(kwargs['id'])
+    return data
 
 incomes_GET = income_GET
 
+def income_POST(*args, **kwargs):
+    id = kwargs['id']
+    obj=json.loads(request.data.decode('utf-8', 'strict'))
+    #try:    
+    i=Income(
+        title=obj['title'],
+        currency_id=int(obj['currency_id']),
+        sum=int(float(obj['sum'])*CURRENCY_SCALE),
+        start_date=datetime.datetime.strptime( obj['start_date'], '%Y-%m-%d').date(),
+        end_date=(None if obj['end_date']=='' else datetime.datetime.strptime( obj['end_date'], '%Y-%m-%d').date()),
+        period_id=int(obj['period_id'])
+    )
+    DB.add(i)
+    DB.flush()
+    DB.commit()
+    #except:
+    #    abort(400)
+    return i
 
 def income_PUT(*args, **kwargs):
     id = kwargs['id']
     i = DB.query(Income).get(id)
     
     obj=json.loads(request.data.decode('utf-8', 'strict'))
-    try:
-        i.title=obj['title']
-        i.currency_id=int(obj['currency_id'])
-        i.sum=int(obj['sum']*CURRENCY_SCALE)
-        i.start_date=datetime.datetime.strptime( obj['start_date'], '%Y-%m-%d').date()
-        i.end_date=(None if obj['end_date']=='' else datetime.datetime.strptime( obj['end_date'], '%Y-%m-%d').date())
-        i.period_id=int(obj['period_id'])
-    except:
-        abort(400)
+    #try:
+    i.title=obj['title']
+    i.currency_id=int(obj['currency_id'])
+    i.sum=int(float(obj['sum'])*CURRENCY_SCALE)
+    i.start_date=datetime.datetime.strptime( obj['start_date'], '%Y-%m-%d').date()
+    i.end_date=(None if obj['end_date']=='' else datetime.datetime.strptime( obj['end_date'], '%Y-%m-%d').date())
+    i.period_id=int(obj['period_id'])
+    #except:
+    #    abort(400)
     DB.commit()
     return {'updated': DB.query(Income).get(id), "previous": i}
 
-
-def income_POST(*args, **kwargs):
+def transaction_DELETE(*args, **kwargs):
     id = kwargs['id']
-    obj=json.loads(request.data.decode('utf-8', 'strict'))
-    try:    
-        i=Income(
-            title=obj['title'],
-            currency_id=int(obj['currency_id']),
-            sum=int(obj['sum']*CURRENCY_SCALE),
-            start_date=datetime.datetime.strptime( obj['start_date'], '%Y-%m-%d').date(),
-            end_date=(None if obj['end_date']=='' else datetime.datetime.strptime( obj['end_date'], '%Y-%m-%d').date()),
-            period_id=int(obj['period_id'])
-        )
-        DB.add(i)
-        DB.flush()
-        DB.commit()
-    except:
-        abort(400)
-    return i
+    income = DB.query(Transaction).filter_by(id=id).delete()
+    DB.commit()
+    return {'deleted': id}
+
+def transaction_GET(*args, **kwargs):
+    """ load intervals from database """
+    return DB.query(Transaction).all() if kwargs['id'] == 0 else DB.query(Transaction).get(kwargs['id'])
 
 def transaction_POST(*args, **kwargs):
     id = kwargs['id']
     obj=json.loads(request.data.decode('utf-8', 'strict'))
     print(obj)
-    try:    
-        i=Transaction(
-            time=datetime.datetime.strptime( obj['time'], '%Y-%m-%d').date(),
-            account_id=int(obj['account_id']),
-            sum=int(obj['sum']*CURRENCY_SCALE),
-            transfer=int(obj['transfer']),
-            income_id=int(obj['income_id']),
-            comment=obj['comment']
-        )
-        DB.add(i)
-        DB.flush()
-        DB.commit()
-    except:
-        abort(400)
+    #try:    
+    i=Transaction(
+        time=datetime.datetime.strptime( obj['time'], '%Y-%m-%d').date(),
+        account_id=int(obj['account_id']),
+        sum=int(float(obj['sum'])*CURRENCY_SCALE),
+        transfer=int(obj['transfer']),
+        income_id=int(obj['income_id']),
+        comment=obj['comment']
+    )
+    DB.add(i)
+    DB.flush()
+    DB.commit()
+    #except:
+    #    abort(400)
     return i
 
 def transaction_PUT(*args, **kwargs):
@@ -260,23 +217,13 @@ def transaction_PUT(*args, **kwargs):
     i.time=datetime.datetime.strptime( obj['time'], '%Y-%m-%d').date()
     i.account_id=int(obj['account_id']) if obj['account_id'] != '' else None
     i.sum=int(float(obj['sum'])*CURRENCY_SCALE)
-    i.transfer=int(obj['transfer']) if obj['transfer'] != '' else None
-    i.income_id=int(obj['income_id']) if obj['income_id'] != '' else None
+    i.transfer=int(obj['transfer']) if obj['transfer'] not in ['0', ''] else None
+    i.income_id=int(obj['income_id']) if obj['income_id'] not in ['0', ''] else None
     i.comment=obj['comment']
     #except:
     #    abort(400)
     DB.commit()
     return {'updated': DB.query(Transaction).get(id), "previous": i}
-
-def transaction_GET(*args, **kwargs):
-    """ load intervals from database """
-    return DB.query(Transaction).all() if kwargs['id'] == 0 else DB.query(Transaction).get(kwargs['id'])
-
-def transaction_DELETE(*args, **kwargs):
-    id = kwargs['id']
-    income = DB.query(Transaction).filter_by(id=id).delete()
-    DB.commit()
-    return {'deleted': id}
 
 def balance_GET(*args, **kwargs):
     r = {}
@@ -330,25 +277,17 @@ def plan_GET(*args, **kwargs):
             }
             }
 
-def transactions_GET(*args, **kwargs):
-    return kwargs, args
 
 
-defaults = {
-        'id': 0,
-        'start_date': '1970-01-01',
-        'end_date': '9999-12-31',
-        'account': 0
-        }
-methods=['GET']
+
+
 s_date=datetime.date.today()
-@app.route('/api', defaults={'api': 'balance'}, methods=methods)
+@app.route('/api', defaults={'api': 'balance'}, methods=['GET'])
 @app.route('/api/<string:api>', defaults={'id': 0}, methods=['GET', 'POST'])
 @app.route('/api/<string:api>/<int:id>', defaults={'end_date': s_date.replace(year=(s_date.year+1))}, methods=['GET', 'DELETE', 'PUT'])
-@app.route('/api/<string:api>/<int:id>/<string:end_date>', defaults={'start_date': s_date.isoformat()}, methods=methods)
-@app.route('/api/<string:api>/<int:id>/<string:start_date>/<string:end_date>', methods=methods)
+@app.route('/api/<string:api>/<int:id>/<string:end_date>', defaults={'start_date': s_date.isoformat()}, methods=['GET'])
+@app.route('/api/<string:api>/<int:id>/<string:start_date>/<string:end_date>', methods=['GET'])
 def dispatcher(*args, **kwargs):
-    
     # print(args, kwargs)
     try:
         start_date = datetime.datetime.strptime(kwargs['start_date'], '%Y-%m-%d').date()
