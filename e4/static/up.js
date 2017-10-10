e5e33f51a66ef50cb2cfd20572e94045d7355d50
Alex Bezpalko <alexbe@wix.com>
2017-10-10 14:00:48 +0300

    update balance_url for 6 months forecast

--- e4/static/up.js
+++ e4/static/up.js
@@ -16,11 +16,35 @@ function update_option(element, url, title_fields=['title']){
   })
 }
 
-function getSunday(d) {
-  d = new Date(d);
+Date.prototype.formatted = function() {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
+
+  return [this.getFullYear(),
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd
+         ].join('-');
+}
+
+function getSunday(_date) {
+  var d = new Date(_date);
   var day = d.getDay(),
       diff = d.getDate() - day; // adjust when day is sunday
-  return new Date(d.setDate(diff));
+  return new Date(d.setDate(diff)).formatted();
+}
+
+function getFirstWeek(_date) {
+  var d = new Date(_date);
+  d.setDate(1);
+  return getSunday(d);
+}
+
+
+function addMonths(_date, _months) {
+  var d = new Date(_date);
+  d.setMonth(d.getMonth() + _months);
+  d.setDate(d.getDate() - 1)
+  return d.formatted();
 }
 
 function getDialogButton( dialog_selector, button_name ) {
