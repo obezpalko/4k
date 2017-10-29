53e2e96a05312dab6c1ef16d6e5952495e1c3e26
Alex Bezpalko <alexbe@wix.com>
2017-10-30 00:21:46 +0200

    some code tidying

--- e4/e4.py
+++ e4/e4.py
@@ -15,7 +15,7 @@ from sqlalchemy import func, and_, or_, desc
 from .utils import number_of_weeks,  strip_numbers
 from .income import DB, Currency, Rate, Income, Interval, Transaction, \
     Account, Payforward
-from .plot import plot_weekly_plan
+#  from .plot import plot_weekly_plan
 
 __version__ = "0.1"
 
--- e4/static/up.js
+++ e4/static/up.js
@@ -5,15 +5,14 @@ function update_option(element, url, title_fields=['title']){
     var options = [];
     var titles;
     $.each(data, function(key, val){
-      if (val['show'] != 'n' ) { 
-      titles = []
-      title_fields.forEach( function(t){ titles.push( (t=='title') ? val[t] : val[t]['title'])});
-      options.push('<option value="'+ val['id'] +'"'+ ((val['default']==1) ? " selected" : "") +'>'+ titles.join(' ') +'</option>')
+      if (val['show'] !== "n" ) { 
+      titles = [];
+      title_fields.forEach( function(t){ titles.push( (t==='title') ? val[t] : val[t]['title']);});
+      options.push('<option value="'+ val['id'] +'"'+ ((val['default']===1) ? " selected" : "") +'>'+ titles.join(" ") +"</option>")
       }
     });
-  
     $(element).html(options.join(''));
-  })
+  });
 }
 
 Date.prototype.formatted = function() {
@@ -21,9 +20,9 @@ Date.prototype.formatted = function() {
   var dd = this.getDate();
 
   return [this.getFullYear(),
-          (mm>9 ? '' : '0') + mm,
-          (dd>9 ? '' : '0') + dd
-         ].join('-');
+          (mm>9 ? "" : "0") + mm,
+          (dd>9 ? "" : "0") + dd
+         ].join("-");
 }
 
 function getSunday(_date) {
@@ -51,7 +50,7 @@ function getDialogButton( dialog_selector, button_name ) {
   var buttons = dialog_selector.find(' .ui-dialog-buttonpane button' );
   for ( var i = 0; i < buttons.length; ++i ) {
      var jButton = $( buttons[i] );
-     if ( jButton.text() == button_name )
+     if ( jButton.text() === button_name )
      {
          return jButton;
      }
@@ -67,7 +66,7 @@ function c_update(url){
     var items = [];
     items.push("<li><span id='currency_refresh' class='ui-icon ui-icon-refresh' onClick='c_update(\"/update_rates\")'></span></li>");
     $.each( data, function( key, val ) {
-      if (val["default"] == "0") {
+      if (val["default"] === "0") {
         items.push( "<li title='" + val["rate_date"] + "' id='currency_" + key + "'>" + val["title"] + ":" + val["rate"] + "</li>" );
       }
     });
@@ -78,12 +77,12 @@ function c_update(url){
 
 function json_names(name, obj){
   // name=a.b.c returns obj {a: {b: {c: value}}}
-  var r = name.split('.');
+  var r = name.split(".");
   var n = r.shift();
   if (! n ) {
     return obj;
   } else {
-    if (! obj) return null;
-    return json_names(r.join('.'), obj[n])
-  };
+    if (! obj) {return null;};
+    return json_names(r.join("."), obj[n])
+  }
 }
